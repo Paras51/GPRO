0797817a7a7d181d35b63ef3ae92fb871529141b
Paras51 <111278801+Paras51@users.noreply.github.com>
2022-11-05 22:30:57 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    
+    extend: {},
+    screens: {
+     'mobile': '360 px',
+     'tablet': '640px',
+     'laptop': '1024px',
+     'desktop': '1280px',
+    }
+  },
+  plugins: [],
+}
